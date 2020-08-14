4ba8d92de6e5eb4675694086c3cb561567ffaadf
iagnes <38885480+iagnes@users.noreply.github.com>
2020-08-14 11:07:49 +0200

    Change on pubspec.yaml and main.dart for new application logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:o2_services/webview.dart';
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
 
 void main() => runApp((MyApp()));
 
