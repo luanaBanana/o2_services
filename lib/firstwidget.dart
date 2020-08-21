1dc570487976ed5b8e1137e36e0230b06e1c2935
iagnes <38885480+iagnes@users.noreply.github.com>
2020-08-21 10:32:29 +0200

    </dict> added in ios/Info.plist File

--- lib/firstwidget.dart
+++ lib/firstwidget.dart
@@ -8,6 +8,9 @@ class FirstWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        color:Colors.blue,
+        ),
       width: double.infinity,
       margin: EdgeInsets.all(20),
         child: Text('This is Google Font Lato \n and our firstWidget', style: GoogleFonts.lato(fontSize: 26,),
