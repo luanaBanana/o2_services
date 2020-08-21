c1108e5e3707d395d4e31d7a04a9c1b17d1bcb62
iagnes <38885480+iagnes@users.noreply.github.com>
2020-08-21 15:55:03 +0200

    webviewpage.dart and urlpage.dart improved Firebase / WebView not working properly yet

--- lib/webviewpage.dart
+++ lib/webviewpage.dart
@@ -1,23 +1,35 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:o2_services/firebase_messaging.dart';
+import 'package:o2_services/firstwidget.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+
+import './firstwidget.dart';
+import './bottomnavigationbar.dart';
+import './webviewpage.dart';
+import './urlpage.dart';
+
 
 class WebViewPage extends StatelessWidget {
 
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+
+  WebViewController _webViewController;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: EdgeInsets.all(15),
-      child: Text(
-        'Add Text',
-        style: TextStyle(fontSize: 28),
-        textAlign: TextAlign.center,
-      ),
 
+    return Expanded(
+        child: WebView(
+          initialUrl: "https://www.google.com",
+          onWebViewCreated: (webViewController) {
+            _webViewController = webViewController;
+          },
+        ),
+      );
 
-
-    );
   }
 }
