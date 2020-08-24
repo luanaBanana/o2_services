7b8e6bce269a25738f3912a3d3593a849d849ace
luiXIV <luana.cusseddu.konto@gmail.com>
2020-08-24 18:45:50 +0200

    Revert "</dict> added in ios/Info.plist File"
    This reverts commit 1dc57048

--- lib/firstwidget.dart
+++ lib/firstwidget.dart
@@ -8,9 +8,6 @@ class FirstWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        color:Colors.blue,
-        ),
       width: double.infinity,
       margin: EdgeInsets.all(20),
         child: Text('This is Google Font Lato \n and our firstWidget', style: GoogleFonts.lato(fontSize: 26,),
